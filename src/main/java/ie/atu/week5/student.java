73f090c1bb9b44a437c5ee20196483d0879f5c6f
NathanPerry057 <G00394355@atu.ie>
2022-10-28 00:01:15 +0100

    Added a student name prompt

--- src/main/java/ie/atu/week5/student.java
+++ src/main/java/ie/atu/week5/student.java
@@ -2,4 +2,10 @@ package ie.atu.week5;
 
 public class student {
 
+    public static void main(String[] args) {
+        System.out.println("Please enter the name of the first student");
+        Scanner input = new Scanner(System.in);
+
+    }
+
 }
